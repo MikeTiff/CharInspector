239b57bac159158c9efb33786bb08f033c13c17b
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-07 17:43:36 -0800

    Character Identifier

--- main.cpp
+++ main.cpp
@@ -5,20 +5,23 @@ using namespace std;
 
 int main()
 {
-
+  char letter;
   cout<<"What character do you want to know about?\n";
-
-  //when user's entry is between A-Z...
+  cin>> letter;
+  if( letter >= 65 && letter <= 90)
+  {
   cout<<" is an upper case letter!\n";
-
-  //when user's entry is between a-z...
+  }
+  else if( letter >= 97 && letter <= 122)
+   {
   cout<<" is a lower case letter!\n";
-
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
+   }
+  else 
+   {
+  cout<<"?! Pssh. What are you talking about?\n";  
+   }
+ 
+  cout<<"The ASCII value is: "<< (int) letter << std::endl;
 
   return 0;
 }
